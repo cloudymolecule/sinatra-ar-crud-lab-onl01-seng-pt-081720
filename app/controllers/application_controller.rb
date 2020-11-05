6fed68cdb3af92dad995730e2c1d0d49886439cc
Cloudymolecule <baires82@gmail.com>
2020-11-05 15:26:45 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -8,6 +8,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  
+
+  get "/articles/new"  do #> Works
+    erb :new
+  end
+
+  post "/articles" do
+    art = Article.create(params)
+    redirect to :"/articles/#{art.id}"
+  end
+
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch "/articles/:id" do
+    @article = Article.find_by(id: params[:id])
+    @article.update(params[:article])
+    @article.save
+    redirect to :"/articles/#{@article.id}"
+  end
+  
+  delete "/articles/:id" do
+    @article = Article.find_by(id: params[:id])
+    @article.destroy
+    redirect to :index
   end
 end
